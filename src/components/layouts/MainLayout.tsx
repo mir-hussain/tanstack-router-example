387328a2c0db27f5ab313f084cfdaec3698720a5
mir-hussain <mir.hm110@gmail.com>
2024-01-08 16:01:20 +0600

    fix: navbar link element

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -1,10 +1,13 @@
+import { Outlet } from '@tanstack/react-router';
 import Navbar from './Navbar';
 
 const MainLayout = () => {
   return (
     <div>
       <Navbar />
-      <h1>Here should be the main outlet</h1>
+      <div className="flex justify-center items-center h-[calc(100vh-80px)]">
+        <Outlet />
+      </div>
     </div>
   );
 };
--- src/components/layouts/Navbar.tsx
+++ src/components/layouts/Navbar.tsx
@@ -9,26 +9,20 @@ import {
 
 const Navbar = () => {
   return (
-    <nav>
-      <NavigationMenu className="h-16 w-full max-w-7xl mx-auto  flex justify-end items-center">
-        <NavigationMenuList className="space-x-3">
-          <NavigationMenuItem>
-            <Link to="/">
-              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                Home
-              </NavigationMenuLink>
-            </Link>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <Link to="/about">
-              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                About
-              </NavigationMenuLink>
-            </Link>
-          </NavigationMenuItem>
-        </NavigationMenuList>
-      </NavigationMenu>
-    </nav>
+    <NavigationMenu className="h-16 w-full max-w-7xl mx-auto  flex justify-end items-center">
+      <NavigationMenuList className="space-x-3">
+        <NavigationMenuItem>
+          <Link to="/" className={navigationMenuTriggerStyle()}>
+            Home
+          </Link>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <Link to="/about" className={navigationMenuTriggerStyle()}>
+            About
+          </Link>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
   );
 };
 
